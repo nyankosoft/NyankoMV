35e1291170cea2f0a2ae23eab26fcf016a98e316
wanko <amorphous.git@gmail.com>
2013-09-29 23:31:58 +0900

    changed: the UI font to Arial

--- Form1.h
+++ Form1.h
@@ -177,6 +177,8 @@ namespace namechanger {
 			this->groupBox1->Controls->Add(this->textBox1);
 			this->groupBox1->Controls->Add(this->label4);
 			this->groupBox1->Controls->Add(this->label3);
+			this->groupBox1->Font = (gcnew System::Drawing::Font(L"Arial", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
 			this->groupBox1->Location = System::Drawing::Point(48, 555);
 			this->groupBox1->Margin = System::Windows::Forms::Padding(4);
 			this->groupBox1->Name = L"groupBox1";
@@ -191,7 +193,7 @@ namespace namechanger {
 			this->checkBox1->AutoSize = true;
 			this->checkBox1->Location = System::Drawing::Point(261, 23);
 			this->checkBox1->Name = L"checkBox1";
-			this->checkBox1->Size = System::Drawing::Size(72, 21);
+			this->checkBox1->Size = System::Drawing::Size(69, 21);
 			this->checkBox1->TabIndex = 5;
 			this->checkBox1->Text = L"Regex";
 			this->checkBox1->UseVisualStyleBackColor = true;
@@ -211,14 +213,14 @@ namespace namechanger {
 			// 
 			this->textBox2->Location = System::Drawing::Point(10, 107);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(323, 24);
+			this->textBox2->Size = System::Drawing::Size(323, 25);
 			this->textBox2->TabIndex = 3;
 			// 
 			// textBox1
 			// 
 			this->textBox1->Location = System::Drawing::Point(10, 50);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(323, 24);
+			this->textBox1->Size = System::Drawing::Size(323, 25);
 			this->textBox1->TabIndex = 2;
 			this->textBox1->TextChanged += gcnew System::EventHandler(this, &Form1::textBox1_TextChanged);
 			// 
@@ -227,7 +229,7 @@ namespace namechanger {
 			this->label4->AutoSize = true;
 			this->label4->Location = System::Drawing::Point(7, 87);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(125, 17);
+			this->label4->Size = System::Drawing::Size(96, 17);
 			this->label4->TabIndex = 1;
 			this->label4->Text = L"Replace with:";
 			// 
@@ -236,7 +238,7 @@ namespace namechanger {
 			this->label3->AutoSize = true;
 			this->label3->Location = System::Drawing::Point(7, 30);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(98, 17);
+			this->label3->Size = System::Drawing::Size(75, 17);
 			this->label3->TabIndex = 0;
 			this->label3->Text = L"Find what:";
 			// 
@@ -253,15 +255,19 @@ namespace namechanger {
 			// label2
 			// 
 			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Arial", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
 			this->label2->Location = System::Drawing::Point(48, 20);
 			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(170, 17);
+			this->label2->Size = System::Drawing::Size(125, 17);
 			this->label2->TabIndex = 4;
 			this->label2->Text = L"Original filenames";
 			// 
 			// button1
 			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Arial", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
 			this->button1->Location = System::Drawing::Point(955, 662);
 			this->button1->Margin = System::Windows::Forms::Padding(4);
 			this->button1->Name = L"button1";
@@ -274,14 +280,18 @@ namespace namechanger {
 			// label5
 			// 
 			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Arial", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
 			this->label5->Location = System::Drawing::Point(588, 20);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(62, 17);
+			this->label5->Size = System::Drawing::Size(57, 17);
 			this->label5->TabIndex = 6;
 			this->label5->Text = L"Prevew";
 			// 
 			// button3
 			// 
+			this->button3->Font = (gcnew System::Drawing::Font(L"Arial", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
 			this->button3->Location = System::Drawing::Point(785, 663);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(163, 36);
